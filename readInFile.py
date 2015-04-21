b4bcb17fa1d2b3c41fdecc3f819b963aee4a3b44
Jared Stensland <jared.stensland@gmail.com>
2015-04-21 11:12:32 -0700

    refactored

--- readInFile.py
+++ readInFile.py
@@ -2,41 +2,23 @@ edgeList = []
 
 #cases: '1test_-10003.txt' 'g1.txt'
 
-with open('g1.txt', 'r') as f:
+with open('test_cases/1test_-10003.txt', 'r') as f:
     heading = f.readline()
     heading = list(map(int,(heading.split())))
-    for line in f:
-        line = line.split()
-        line = list(map(int, line))
-        edgeList.append(line)
+
+    edgeList = [list(map(int, line.split())) for line in f]
+
+#TEST THE SPEED DIFFERENCE FOR THIS BELOW COMPARED TO ABOVE.
+# ABOVE SUPPOSEDLY IS FASTER BECAUSE IT USES LIST COMPRESIONS
+# WHICH ARE SUPPOSED TO BE FASTER THAN APPENDING IN A LOOP
+
+##    for line in f:
+##        lineStrArr = line.split()
+##        lineIntArr = list(map(int, lineStrArr))
+##        edgeList.append(lineIntArr)
+
+    
 
 print (heading)
 print (edgeList)
 
-##with open('g1.txt', 'r') as f:
-##    data = f.readlines()
-##
-##    for line in data:
-##        words = line.split()
-##        
-
-###read single line
-##a = f.readline()
-##
-##print (a)
-##
-###read entire file
-##b = f.read()
-##
-###print (b)
-##
-### good to read it in one line at a time.
-##f = open('workfile', 'w')
-##for line in f:
-##    print(line, end='')
-##
-##
-### better with a with statement
-##with open('workfile', 'w') as f:
-##    for line in f:
-##        print(line, end='')
