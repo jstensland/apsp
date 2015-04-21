b7d9d5713c67bb2d82d303c8c77508027819e95a
Jared Stensland <jared.stensland@gmail.com>
2015-04-21 11:28:31 -0700

    more refactoring

--- readInFile.py
+++ readInFile.py
@@ -2,13 +2,17 @@ edgeList = []
 
 #cases: '1test_-10003.txt' 'g1.txt'
 
-with open('test_cases/1test_-10003.txt', 'r') as f:
-    heading = f.readline()
-    heading = list(map(int,(heading.split())))
 
-    edgeList = [list(map(int, line.split())) for line in f]
+def lineToIntArr(line):
+    lineStrArr = line.split()
+    lineIntArr = list(map(int, lineStrArr))
+    return lineIntArr
 
-#TEST THE SPEED DIFFERENCE FOR THIS BELOW COMPARED TO ABOVE.
+#One line should be fast but less readable?
+    
+#edgeList = [list(map(int, line.split())) for line in f]
+
+# TEST THE SPEED DIFFERENCE FOR THIS BELOW COMPARED TO ABOVE/below.
 # ABOVE SUPPOSEDLY IS FASTER BECAUSE IT USES LIST COMPRESIONS
 # WHICH ARE SUPPOSED TO BE FASTER THAN APPENDING IN A LOOP
 
@@ -17,7 +21,12 @@ with open('test_cases/1test_-10003.txt', 'r') as f:
 ##        lineIntArr = list(map(int, lineStrArr))
 ##        edgeList.append(lineIntArr)
 
-    
+
+with open('test_cases/1test_-10003.txt', 'r') as f:
+    heading = f.readline()
+    heading = list(map(int,(heading.split())))
+
+    edgeList = [lineToIntArr(line) for line in f]
 
 print (heading)
 print (edgeList)
