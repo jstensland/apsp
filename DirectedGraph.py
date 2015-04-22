1ff79bfdcf50774b600cd72f2e37f5cc34e52f38
Jared Stensland <jared.stensland@gmail.com>
2015-04-21 17:42:28 -0700

    added more test cases and completed algorithm, working on first three test cases

--- DirectedGraph.py
+++ DirectedGraph.py
@@ -1,3 +1,7 @@
+#refactor to make some methods private, that should never be called on a graph alone.
+#refactor to re-order i, j and k such that in the end you can just find the mine for the last
+#2D matrix
+
 import pprint
 class DirectedGraph(object):
  #   import pprint
@@ -6,7 +10,7 @@ class DirectedGraph(object):
         self.edgeList = edgeList
         self.nodeCount = nodeCount
         self.edgeCount = edgeCount
-        self.pathLengths = []
+        self.pathLengthsMatrix = []
 
     def __str__(self):
         edgeStrList = list(map(str, self.edgeList))
@@ -16,14 +20,39 @@ class DirectedGraph(object):
         objectId = self.__repr__()
         return nodeCount + edgeCount + edges + objectId
 
+    def findShortestShortestPath(self):
+        self.asapFloydWarshall()
+        if self.negativeCycleCheck():
+            return "negative Cycle!"
+        else:
+            return self.shortestShortestPathFW()
+
+    def negativeCycleCheck(self):
+        negativeCycle = False
+        for i in range(self.nodeCount):
+            if self.pathLengthsMatrix[i][i][self.nodeCount-1] != 0:
+                negativeCycle = True
+        return negativeCycle
+
+    def shortestShortestPathFW(self):
+        shortest = 100000000
+        for i in range(self.nodeCount):
+            for j in range(self.nodeCount):
+                if self.pathLengthsMatrix[i][j][self.nodeCount-1] != None:
+                    if self.pathLengthsMatrix[i][j][self.nodeCount-1] < shortest:
+                        shortest = self.pathLengthsMatrix[i][j][self.nodeCount-1]
+        return shortest
+
     def asapFloydWarshall(self):
-        self.pathLengths = [[[self.initializeArrayValue(i, j, k)
+        #initialize Matrix
+        self.pathLengthsMatrix = [[[self.initializeArrayValue(i, j, k)
                               for k in range(self.nodeCount)]
                              for j in range(self.nodeCount)]
                             for i in range(self.nodeCount)]
         self.addInitialEdgeValues()
+        self.calculatePathLengthsMatrix()
         
-        pprint.pprint(self.pathLengths)
+        pprint.pprint(self.pathLengthsMatrix)
 
     def initializeArrayValue(self, i, j, k):
         initialValue = None
@@ -33,5 +62,33 @@ class DirectedGraph(object):
 
     def addInitialEdgeValues(self):
         for edge in self.edgeList:
-            self.pathLengths[edge[0]][edge[1]][0] = edge[2]
+            self.pathLengthsMatrix[edge[0]][edge[1]][0] = edge[2]
+
+    def calculatePathLengthsMatrix(self):
+        for k in range(1, self.nodeCount):
+            for i in range(self.nodeCount):
+                for j in range(self.nodeCount):
+                    #print ([i, j, k])
+                    self.pathLengthsMatrix[i][j][k] = self.newPathLength(i, j, k)
+
+    def newPathLength(self, i, j, k):
+        #if nodes not connected and k doesn't connect them, still unconnected.
+        if self.pathLengthsMatrix[i][j][k-1] != None:
+            case1 = self.pathLengthsMatrix[i][j][k-1]
+        else:
+            case1 = None
+                
+        if self.pathLengthsMatrix[i][k][k-1] != None and self.pathLengthsMatrix[k][j][k-1] != None:
+            case2 = self.pathLengthsMatrix[i][k][k-1] + self.pathLengthsMatrix[k][j][k-1]
+        else:
+            case2 = None
+
+        if case1 == None and case2 == None:
+            return None
+        elif case1 == None:
+            return case2
+        elif case2 == None:
+            return case1
+        else:
+            return min(case1, case2)
         
--- asapAlogrithms.py
+++ asapAlogrithms.py
@@ -6,16 +6,18 @@ graphReader = GraphReader.GraphReader()
 
 #cases: 'test_cases/1test_-10003.txt'
 #       'test_cases/2test_-6.txt'
+#       'test_cases/3test_negcycle.txt'
 #       'test_cases/g1.txt'
-#       
-file = 'test_cases/1test_-10003.txt'
+
+file =  'test_cases/1test_-10003.txt'
 headerSize = 1
 header = graphReader.readHeader(file, headerSize) # [nodeCount, edgeCount]
 edgeList = graphReader.readNodes(file, headerSize) # [head, tail, weight]
 
 graph = DirectedGraph.DirectedGraph(edgeList, header[0][0], header[0][1])
 
-graph.asapFloydWarshall()
+#graph.asapFloydWarshall()
+print (graph.findShortestShortestPath())
 
 #print (graph)
 
