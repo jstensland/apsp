ae947efe672daabcb1de953d4a15fcd2405b76f5
Jared Stensland <jared.stensland@gmail.com>
2015-06-02 18:55:49 -0700

    deleted and inserted some white spaces

--- asapAlogrithms.py
+++ asapAlogrithms.py
@@ -3,7 +3,7 @@ import DirectedGraph
 
 graphReader = GraphReader.GraphReader()
 
-#cases: 'test_cases/1test_-10003.txt'
+# cases: 'test_cases/1test_-10003.txt'
 #       'test_cases/2test_-6.txt'
 #       'test_cases/3test_negcycle.txt'
 #       'test_cases/4test_larger_negativeCycle.txt'
@@ -15,23 +15,23 @@ graphReader = GraphReader.GraphReader()
 #       'test_cases/g3.txt'
 
 file = 'test_cases/hwQ4test.txt'
-print ('File: ' + file)
+print('File: ' + file)
 headerSize = 1
-header = graphReader.readHeader(file, headerSize) # [nodeCount, edgeCount]
-edgeList = graphReader.readNodes(file, headerSize) # [head, tail, weight]
+header = graphReader.readHeader(file, headerSize)  # [nodeCount, edgeCount]
+edgeList = graphReader.readNodes(file, headerSize)  # [head, tail, weight]
 
-print( "read graph")
+print("read graph")
 graph = DirectedGraph.DirectedGraph(edgeList, header[0][0], header[0][1])
-print( "created Graph")
-#graph.asapFloydWarshall()
+print("created Graph")
+# graph.asapFloydWarshall()
 
-print (graph.findShortestShortestPath())
+print(graph.findShortestShortestPath())
 
-#print (graph)
+# print (graph)
 
-#import cProfile
-#import re
-#cProfile.run('graph.findShortestShortestPath()')
+# import cProfile
+# import re
+# cProfile.run('graph.findShortestShortestPath()')
 
 
-#print ([method for method in dir(graph) if callable(getattr(graph, method))])
+# print ([method for method in dir(graph) if callable(getattr(graph, method))])
